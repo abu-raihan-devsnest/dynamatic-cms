750a556c11543b4ee8323717affb606de2830968
Abu Raihan <raihanahmed01973@gmail.com>
2024-07-08 15:44:37 +0600

    updated headerText file

--- src/components/HeaderTitle.jsx
+++ src/components/HeaderTitle.jsx
@@ -1,11 +1,18 @@
 import { Box, Img, Text } from "@chakra-ui/react";
-import leftArrow from "../assets/icons/leftArrow.png";
+import leftArrow from "../assets/icons/leftArrow.svg";
 const HeaderTitle = ({ titleText }) => {
   return (
     <Box>
-      <Box>
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        gap={"10px"}
+        fontFamily={'"Inter", sens-serif'}
+      >
         <Img src={leftArrow} />
-        <Text>{titleText}</Text>
+        <Text color={"#000000"} fontWeight={"600"} fontSize={"18px"}>
+          {titleText}
+        </Text>
       </Box>
     </Box>
   );
